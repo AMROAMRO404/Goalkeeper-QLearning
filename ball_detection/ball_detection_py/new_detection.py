d40e695a97bc8fcc9337ecab04ad80f02f797238
AMROAMRO404 <amro.amro1999@gmail.com>
2022-02-16 16:42:52 +0200

    adjust code

--- ball_detection/ball_detection_py/new_detection.py
+++ ball_detection/ball_detection_py/new_detection.py
@@ -1,6 +1,8 @@
 import cv2
 from cv2 import imshow
 import numpy as np
+
+
 cap = cv2.VideoCapture(0)
 while True:
     _, frame = cap.read()
@@ -14,23 +16,31 @@ while True:
     mask = cv2.inRange(hsv_frame, ORANGE_MIN, ORANGE_MAX)
 
     countours, _ = cv2.findContours(
-        mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        mask,
+        cv2.RETR_TREE,
+        cv2.CHAIN_APPROX_SIMPLE
+    )
+
     countours = sorted(
-        countours, key=lambda x: cv2.contourArea(x), reverse=True)
+        countours,
+        key=lambda x: cv2.contourArea(x),
+        reverse=True
+    )
 
     x_medium = 0
     y_medium = 0
 
     for cnt in countours:
         (x, y, w, h) = cv2.boundingRect(cnt)
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
         x_medium = x + int(w/2)
         y_medium = y + int(h/2)
         print("x_medium = ", x_medium, ", y_medium = ", y_medium)
-        cv2.line(frame, (x_medium, 0),
-                 (x_medium, rows), (0, 255, 0), 2)
-        cv2.line(frame, (0, y_medium),
-                 (cols, y_medium), (0, 255, 0), 2)
+
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.line(frame,  (x_medium, 0), (x_medium, rows), (0, 255, 0), 2)
+        cv2.line(frame, (0, y_medium), (cols, y_medium), (0, 255, 0), 2)
+
         break
 
     cv2.imshow("Frame", frame)
