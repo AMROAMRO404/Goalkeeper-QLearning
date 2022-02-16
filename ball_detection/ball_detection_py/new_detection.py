8bd75a2d38ff943a9ca44f79d9d42ef64fc4480d
AMROAMRO404 <amro.amro1999@gmail.com>
2022-02-16 21:03:28 +0200

    add border

--- ball_detection/ball_detection_py/new_detection.py
+++ ball_detection/ball_detection_py/new_detection.py
@@ -3,6 +3,17 @@ from cv2 import imshow
 import numpy as np
 
 
+x_medium = 0
+y_medium = 0
+
+border_x = 20
+border_y = 20
+
+border_color = (255, 0, 0)
+ball_border_color = (0, 255, 0)
+
+thikness = 2
+
 cap = cv2.VideoCapture(0)
 while True:
     _, frame = cap.read()
@@ -10,7 +21,7 @@ while True:
 
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    ORANGE_MIN = np.array([10, 100, 80], np.uint8)
+    ORANGE_MIN = np.array([10, 100, 120], np.uint8)
     ORANGE_MAX = np.array([25, 255, 255], np.uint8)
 
     mask = cv2.inRange(hsv_frame, ORANGE_MIN, ORANGE_MAX)
@@ -27,19 +38,39 @@ while True:
         reverse=True
     )
 
-    x_medium = 0
-    y_medium = 0
+    rows = rows - border_y
+    cols = cols - border_x
+
+    # draw rectangle
+    cv2.line(frame,  (border_x, border_y), (border_x, rows),
+             border_color, thikness)
+
+    cv2.line(frame,  (border_x, border_y), (cols, border_y),
+             border_color, thikness)
+
+    cv2.line(frame,  (cols, border_y), (cols, rows),
+             border_color, thikness)
+
+    cv2.line(frame,  (border_x, rows), (cols, rows),
+             border_color, thikness)
 
     for cnt in countours:
         (x, y, w, h) = cv2.boundingRect(cnt)
 
-        x_medium = x + int(w/2)
-        y_medium = y + int(h/2)
-        print("x_medium = ", x_medium, ", y_medium = ", y_medium)
+        x_medium = (x + w//2)
+        y_medium = (y + h//2)
 
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        cv2.line(frame,  (x_medium, 0), (x_medium, rows), (0, 255, 0), 2)
-        cv2.line(frame, (0, y_medium), (cols, y_medium), (0, 255, 0), 2)
+        cv2.rectangle(frame, (x, y), (x + w, y + h),
+                      ball_border_color, thikness)
+        cv2.line(frame,  (x_medium, border_y),
+                 (x_medium, rows), ball_border_color, thikness)
+        cv2.line(frame, (border_x, y_medium),
+                 (cols, y_medium), ball_border_color, thikness)
+
+        # the values that goes to RL model
+        x_medium = x_medium - border_x
+        y_medium = y_medium - border_y
+        print("x_medium = ", x_medium, ", y_medium = ", y_medium)
 
         break
 
